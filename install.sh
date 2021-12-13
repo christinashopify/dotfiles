a9ebc3c8cae835383f323856b659bb06d66d824b
Christina Stewart <christina.stewart@shopify.com>
2021-12-13 17:22:30 +0100

    install vimplug

--- install.sh
+++ install.sh
@@ -8,3 +8,5 @@ sudo apt-get install -y silversearcher-ag
 sudo apt-get install -y automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev 
 sudo apt-get purge neovim vim 
 sudo apt-get install vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +qall
