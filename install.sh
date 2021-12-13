fe3e5a090d57f0b57122e9264c634a1d51093238
Christina Stewart <christina.stewart@shopify.com>
2021-12-13 18:05:26 +0100

    debugging

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ fi
 
 sudo apt-get install -y silversearcher-ag
 sudo apt-get install -y automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev
-sudo apt-get purge neovim vim 
-sudo apt-get install vim
-#curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#sudo apt-get purge neovim vim 
+#sudo apt-get install vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 #vim +PlugInstall +qall
