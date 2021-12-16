68baec401103c9393f1e9a5b2a038cf092038885
Christina Stewart <christina.stewart@shopify.com>
2021-12-16 17:04:46 +0100

    try new approach

--- .zshrc
+++ .zshrc
@@ -4,3 +4,5 @@ alias gst="git status"
 
 export FZF_DEFAULT_COMMAND='ag -l --path-to-ignore ~/.agignore'
 export EDITOR=vim
+export GEM_HOME=/home/spin/.bundle/$(/bin/ls ~/src/*/*)
+export GEM_PATH=/home/spin/.bundle/$(/bin/ls ~/src/*/*)
--- install.sh
+++ install.sh
@@ -12,10 +12,8 @@ sudo update-alternatives --remove vim /usr/bin/nvim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qall
 
+
 # tag repo
-export GEM_HOME=/home/spin/.bundle/$(/bin/ls ~/src/*/*)
 gem install ripper-tags
-(
 cd ~/src/*/*/*
 ripper-tags -R --exclude=vendor
-)
